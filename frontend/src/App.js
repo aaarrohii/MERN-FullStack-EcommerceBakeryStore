a8400aa39356066f96c7bda8eedad4e19dda54d7
aaarrohii <142020773+aaarrohii@users.noreply.github.com>
2023-09-23 00:19:26 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,46 +1,38 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { setDataProduct } from './redux/productSlice';
 import Header from './component/Header';
+import './App.css';
 import { Outlet } from 'react-router-dom';
-import toast, { Toaster } from 'react-hot-toast';
-import axios from 'axios';
+import toast, {Toaster} from "react-hot-toast";
+import { useEffect } from 'react';
+import { setDataProduct } from './redux/productSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
-  const dispatch = useDispatch();
-  const productData = useSelector((state) => state.product);
 
-  useEffect(() => {
-    (async () => {
-      try {
-        const res = await fetch(`${process.env.REACT_APP_SERVER_DOMIN}/product`);
-        const resData = await res.json();
-        dispatch(setDataProduct(resData));
-      } catch (error) {
-        console.error('Error fetching product data:', error);
-      }
-    })();
-  }, []);
+  const dispatch=useDispatch()
+  const productData=useSelector((state)=>state.product)
 
-
+  useEffect(()=>{
+    (async()=>{
+      const res= await fetch(`${process.env.REACT_APP_SERVER_DOMIN}/product`)
+      const resData=await res.json()
+      dispatch(setDataProduct(resData))
+    })()
+  },[])
+ 
   return (
-    <>
-      <Toaster />
-      <div>
-        <Header />
-        <main className='pt-20 bg-pink-100 min-h-[calc(100vh)]'>
-          <form onSubmit={handleSubmit}>
-            {/* Add your form fields here */}
-            <button type='submit'>Submit</button>
-          </form>
-          <Outlet />
-        </main>
+  <>
+  <Toaster/>
+  <div>
+    <Header/>
+    <main className='pt-20 bg-pink-100 min-h-[calc(100vh)] '>
+      <Outlet/>
+      </main>
       </div>
-    </>
-  );
+      </>
+  )
 }
-
 export default App;
+
 
 
 
