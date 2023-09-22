e5367390787975a3a7ea3c5aebc529f0f71cedc1
aaarrohii <142020773+aaarrohii@users.noreply.github.com>
2023-09-22 15:56:35 +0530

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,20 +5,11 @@ import toast, {Toaster} from "react-hot-toast";
 import { useEffect } from 'react';
 import { setDataProduct } from './redux/productSlice';
 import { useDispatch, useSelector } from 'react-redux';
-import axios from 'axios'
 
 function App() {
 
   const dispatch=useDispatch()
   const productData=useSelector((state)=>state.product)
-
-  axios.defaults.withCredentials=true;
-  const handleSubmit=(e)=>{
-    e.preventDefault();
-    axios.post('https://mern-full-stack-ecommerce-bakery-store-server.vercel.app/login',{email,password})
-    .then(result=>console.log(result))
-    .catch(err=>console.log(err))
-  }
 
   useEffect(()=>{
     (async()=>{
