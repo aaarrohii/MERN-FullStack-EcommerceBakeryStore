c12ad85ac52e4f8105df8d3fbbfb9a0d5df8366e
aaarrohii <142020773+aaarrohii@users.noreply.github.com>
2023-09-22 23:34:38 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,68 +1,42 @@
-import Header from './component/Header';
-import './App.css';
-import { Outlet } from 'react-router-dom';
-import toast, {Toaster} from "react-hot-toast";
-import { useEffect } from 'react';
-import { setDataProduct } from './redux/productSlice';
-import { useDispatch, useSelector } from 'react-redux';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./component/Header";
+import { Outlet } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
+import { useEffect } from "react";
+import { setDataProduct } from "./redux/productSlide";
+import { useDispatch, useSelector } from "react-redux";
 import axios from 'axios'
 
 function App() {
-
-  const dispatch=useDispatch()
-  const productData=useSelector((state)=>state.product)
-
-useEffect(() => {
-  (async () => {
-    try {
-      const res = await fetch(`${process.env.REACT_APP_SERVER_DOMIN}/product`);
-      if (!res.ok) {
-        throw new Error(`Failed to fetch data: ${res.status}`);
-      }
-      const resData = await res.json();
-      dispatch(setDataProduct(resData));
-    } catch (error) {
-      console.error('Error fetching product data:', error);
-    }
-  })();
-}, []);
-  axios.defaults.withCredentials=true;
-   const handleSubmit = async () => {
-      try {
-        // Replace 'https://your-backend-url.com' with the actual URL of your backend API
-        const backendUrl = 'https://mern-full-stack-ecommerce-bakery-store.vercel.app/';
-
-        // Define the data you want to send in the POST request
-        const requestData = {
-          // Include your request data here
-        };
-
-        // Make a POST request to the backend URL
-        const response = await axios.post(`${backendUrl}/login`, requestData);
-
-        // Handle the response data
-        console.log('Response from server:', response.data);
-      } catch (error) {
-        // Handle any errors that occur during the request
-        console.error('Error while making the POST request:', error);
-      }
-    };
-
-    // Call the handleSubmit function when the component mounts or when you want to trigger the request
-    handleSubmit();
-  }, []);
-    
+  const dispatch = useDispatch()
+  const productData = useSelector((state)=>state.product)
+ 
+  useEffect(()=>{
+    (async()=>{
+      const res = await fetch(`${process.env.REACT_APP_SERVER_DOMIN}/product`)
+      const resData = await res.json()
+      dispatch(setDataProduct(resData))
+    })()
+  },[])
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    axios.post('https://frostedbites-ecommerce-mern-fullstack-project.vercel.app/')
+    .then(result=>console.log(result))
+    .catch(err=>console.log(err))
+  }
 
   return (
-  <>
-  <Toaster/>
-  <div>
-    <Header/>
-    <main className='pt-20 bg-pink-100 min-h-[calc(100vh)] '>
-      <Outlet/>
-      </main>
+    <>
+      <Toaster />
+      <div>
+        <Header />
+        <main className="pt-16 bg-slate-100 min-h-[calc(100vh)]">
+          <Outlet />
+        </main>
       </div>
-      </>
-  )
+    </>
+  );
 }
+
 export default App;
