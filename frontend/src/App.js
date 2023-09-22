d9b2a5b7ea4c5325cb5a21636f2a639b98e28843
aaarrohii <142020773+aaarrohii@users.noreply.github.com>
2023-09-22 16:01:15 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import toast, {Toaster} from "react-hot-toast";
 import { useEffect } from 'react';
 import { setDataProduct } from './redux/productSlice';
 import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios'
 
 function App() {
 
@@ -18,6 +19,15 @@ function App() {
       dispatch(setDataProduct(resData))
     })()
   },[])
+
+  axios,defaults.withCredentials=true;
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    axios.post("https://mern-full-stack-ecommerce-bakery-store.vercel.app/login",{email,password})
+    .then(result=>console.log(result))
+    .catch(err=>console.log(err))
+  }
+    
 
   return (
   <>
