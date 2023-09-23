1b7338e6cdd7f0f182bc8355f0fae57cd0fa32a2
aaarrohii <142020773+aaarrohii@users.noreply.github.com>
2023-09-23 14:09:02 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
 import Header from './component/Header';
 import './App.css';
-import {useState } from "react";
 import { Outlet } from 'react-router-dom';
 import toast, {Toaster} from "react-hot-toast";
 import { useEffect } from 'react';
 import { setDataProduct } from './redux/productSlice';
 import { useDispatch, useSelector } from 'react-redux';
-import axios from 'axios';
+import axios from 'axios'
 
 function App() {
 
@@ -21,65 +20,22 @@ function App() {
     })()
   },[])
 
-  axios.defaults.withCredentials = true;
-  const [formData, setFormData] = useState({
-    name: '',
-    price: 0,
-  });
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-]
-    axios
-      .post(`${process.env.REACT_APP_SERVER_DOMIN}/product`, formData)
-      .then((response) => {
-        console.log('Response from backend:', response.data);
-
-        toast.success('Product added successfully')
-        setFormData({
-          name: '',
-          price: 0,]
-        });
-      })
-      .catch((error) => {
-        console.error('Error adding product:', error);
-        toast.error('Error adding product');
-      });
-  };
-   return (
-    <>
-      <Toaster />
-      <div>
-        <Header />
-        <main className='pt-20 bg-pink-100 min-h-[calc(100vh)] '>
-          {/* Your existing code */}
-          <Outlet />
-
-          {/* Example form */}
-          <form onSubmit={handleSubmit}>
-            <input
-              type='text'
-              placeholder='Product Name'
-              name='name'
-              value={formData.name}
-              onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-            />
-            <input
-              type='number'
-              placeholder='Product Price'
-              name='price'
-              value={formData.price}
-              onChange={(e) => setFormData({ ...formData, price: e.target.value })}
-            />
-            <button type='submit'>Add Product</button>
-          </form>
-        </main>
+  
+ 
+  return (
+  <>
+  <Toaster/>
+  <div>
+    <Header/>
+    <main className='pt-20 bg-pink-100 min-h-[calc(100vh)] '>
+      <Outlet/>
+      </main>
       </div>
-    </>
-  );
+      </>
+  )
 }
-
 export default App;
+
 
 
 
