6f1fbf4e4a4dd34666fef571a282976681e58c77
aaarrohii <142020773+aaarrohii@users.noreply.github.com>
2023-09-23 13:19:57 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import toast, {Toaster} from "react-hot-toast";
 import { useEffect } from 'react';
 import { setDataProduct } from './redux/productSlice';
 import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
 
 function App() {
 
@@ -18,6 +19,44 @@ function App() {
       dispatch(setDataProduct(resData))
     })()
   },[])
+
+  axios.defaults.withCredentials = true;
+
+  const [formData, setFormData] = useState({
+    // Initialize your form fields here
+    name: '',
+    price: 0,
+    // Add more fields as needed
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Make a POST request to your backend
+    axios
+      .post(`${process.env.REACT_APP_SERVER_DOMAIN}/product`, formData)
+      .then((response) => {
+        // Handle the response from the backend here
+        console.log('Response from backend:', response.data);
+        // You can update your Redux state or perform other actions as needed
+
+        // Show a success message or perform any other UI updates
+        toast.success('Product added successfully');
+
+        // Clear the form or perform any other necessary actions
+        setFormData({
+          name: '',
+          price: 0,
+          // Clear other form fields here
+        });
+      })
+      .catch((error) => {
+        // Handle any errors here
+        console.error('Error adding product:', error);
+        // Show an error message or perform other error handling as needed
+        toast.error('Error adding product');
+      });
+  };
  
   return (
   <>
